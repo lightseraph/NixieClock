78bfe581e874453931c1cecc770409cd32b34b42
light <lightseraph@163.com>
2023-02-15 18:45:39 +0800

    2.15.1

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,20 @@
 #include <Arduino.h>
 #include <AutoConnect.h>
 #include <DS3231.h>
+#include <FastLED.h>
+
+/*----------LED PARAM-----------*/
+#define NUM_LEDS 2
+#define LED_TYPE WS2812
+#define COLOR_ORDER GRB
+#define DATA_PIN 16
+#define LED_BRIGHTNESS 128
 
 WebServer Server;
 AutoConnect Portal(Server);
 AutoConnectConfig config;
+
+CRGB leds[NUM_LEDS];
 
 void rootPage()
 {
@@ -23,10 +33,16 @@ void setup()
   Server.on("/", rootPage);
   Portal.begin();
   // Serial.println("Web server started: " + Wifi.localIP().toString());
+
+  LEDS.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+  FastLED.setBrightness(LED_BRIGHTNESS);
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
   Portal.handleClient();
+  fill_solid(leds, 2, CRGB::OrangeRed);
+  FastLED.show();
+  delay(25);
 }
