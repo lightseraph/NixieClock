30da790068aadc5c48150dbaf5785e25c5521142
light <lightseraph@163.com>
2023-03-07 19:20:30 +0800

    3.7.1

--- include/Hardware.h
+++ include/Hardware.h
@@ -8,14 +8,14 @@
 /*---------------LED----------------*/
 
 #define NUM_LEDS 4
-#define LED_TYPE WS2812
+#define LED_TYPE WS2812B
 #define COLOR_ORDER GRB
 #define DATA_PIN 26
 #define LED_BRIGHTNESS 40
 
 /*-------------Nixie---------------*/
 
-#define NUM_PIN_A 32
+#define NUM_PIN_A 34
 #define NUM_PIN_B 33
 #define HV_ENABLE 27
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Hardware.h>
 #include <network.h>
+#include <Preferences.h>
 
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "ntp1.aliyun.com", 28800, 86400000);
@@ -9,6 +10,11 @@ CRGB leds[NUM_LEDS];
 IRrecv irrecv(IR_PIN);
 
 decode_results results;
+Preferences pref;
+
+CHSV color;
+
+void flash_led();
 
 void setup()
 {
@@ -28,37 +34,140 @@ void setup()
   digitalWrite(NUM_PIN_A, LOW);
   digitalWrite(NUM_PIN_B, LOW);
 
+  pref.begin("pref");
+  color.hue = pref.getUChar("hue", 0);
+  color.sat = pref.getUChar("sat", 255);
+  color.val = pref.getUChar("val", 255);
+
   // startWifiWithWebServer();
   irrecv.enableIRIn();
 }
-int i = 0;
 void loop()
 {
   // put your main code here, to run repeatedly:
+  static uint8_t work_status = 1;
 
-  delay(25);
   if (irrecv.decode(&results))
   {
     // print() & println() can't handle printing long longs. (uint64_t)
     serialPrintUint64(results.command, HEX);
 
     Serial.println("");
-    leds[i] = CRGB::Black;
-    if (results.command == 0x40 && i != 3)
+    if (results.command == 0x43) // play/pause
     {
-      i++;
+      work_status = 1;
     }
-    if (results.command == 0x44 && i > 0)
+    if (results.command == 0x44) // prev
     {
-      i--;
+      if (work_status == 1)
+        work_status = 2;
+      else
+        work_status = 1;
     }
-    if (results.command == 0x45)
-      i = -1;
+    if (results.command == 0x40) // next
+    {
+      if (work_status == 1)
+        work_status = 3;
+      else
+        work_status = 1;
+    }
+    if (results.command == 0x09) // EQ
+    {
+      pref.putUChar("hue", color.hue);
+      pref.putUChar("sat", color.sat);
+      pref.putUChar("val", color.val);
+      flash_led();
+    }
+    if (results.command == 0x15) // ++++
+    {
+      if (color.val < 245)
+        color.val += 10;
+      else
+        color.val = 255;
+    }
+    if (results.command == 0x07) // ----
+    {
+      if (color.val > 10)
+        color.val -= 10;
+      else
+        color.val = 0;
+    }
+    if (results.command == 0x47) // CH-
+    {
+      if (color.sat < 245)
+        color.sat += 10;
+      else
+        color.sat = 255;
+    }
+    if (results.command == 0x45) // CH+
+    {
+      if (color.sat > 10)
+        color.sat -= 10;
+      else
+        color.sat = 0;
+    }
+    if (results.command == 0x46) // CH
+    {
+      color.hue = pref.getUChar("hue", 0);
+      color.sat = pref.getUChar("sat", 255);
+      color.val = pref.getUChar("val", 255);
+    }
+    if (results.command == 0x4a) // 9 --- HV
+      digitalWrite(HV_ENABLE, ~digitalRead(HV_ENABLE));
+
+    if (results.command == 0x0C) // 1 ---
+    {
+      digitalWrite(NUM_PIN_A, HIGH);
+      digitalWrite(NUM_PIN_B, LOW);
+    }
+
+    if (results.command == 0x18) // 2 ---
+    {
+      digitalWrite(NUM_PIN_A, LOW);
+      digitalWrite(NUM_PIN_B, HIGH);
+    }
+
+    if (results.command == 0x16) // 0 ---
+    {
+      digitalWrite(NUM_PIN_A, LOW);
+      digitalWrite(NUM_PIN_B, LOW);
+    }
+
+    fill_solid(leds, 4, CRGB::Black);
+    FastLED.show();
+    delay(30);
     irrecv.resume(); // Receive the next value
-    Serial.println(i);
   }
-  leds[i] = CRGB::Red;
+
+  switch (work_status)
+  {
+  case 0:
+    break;
+  case 1:
+    break;
+  case 2:
+    color.hue--;
+    break;
+  case 3:
+    color.hue++;
+    break;
+  }
+
+  fill_solid(leds, 4, color);
   FastLED.show();
 
-  delay(50);
+  delay(100);
+}
+
+void flash_led()
+{
+  for (int i = 0; i < 5; i++)
+  {
+    fill_solid(leds, 4, CRGB::Black);
+    FastLED.show();
+    delay(50);
+    fill_solid(leds, 4, color);
+    FastLED.show();
+    delay(50);
+  }
 }
